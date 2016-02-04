75da491b7a8fb22f8fc8f7ba41a82a7d10abe7c5
Djiedjom Aniakou <pacom20003@hotmail.com>
2016-02-04 11:17:27 -0500

    Adding test class

---
+++ src/test/java/com/git/project/test/StringTest.java
@@ -0,0 +1,10 @@
+package com.git.project.test;
+
+public class StringTest {
+
+	public static void main(String[] args) {
+		String concatString = String.join(" ", "Hello", "World");
+		System.out.println(concatString);
+	}
+
+}
